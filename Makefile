eae7f9f8ca9e3818e3cecb7190aa958a5b5f1e1f
Grant Hernandez <grant.h.hernandez@gmail.com>
2015-07-28 20:56:43 -0400

    Mostly working control module

--- Makefile
+++ Makefile
@@ -2,16 +2,11 @@
 PROGRAM=control-module
 SOURCES=main.cpp
 
-LIBRARIES=lib/MEGA32A_UART_LIBRARY/MEGA32A_UART_LIBRARY/ \
-	  lib/MEGA32A_SPI_LIBRARY/MEGA32A_SPI_LIBRARY/ \
-	  lib/WHCS_RF24/WHCS_RF24/ \
-	  common/
-
-INCLUDE=$(LIBRARIES)
-LIB_INCLUDE=$(LIBRARIES)
-
-# include any libraries
-LDFLAGS=-lcommon -lUART -lRF24 -lSPI
+LIBRARIES=lib/MEGA32A_UART_LIBRARY/MEGA32A_UART_LIBRARY/libUART.a \
+	  lib/MEGA32A_SPI_LIBRARY/MEGA32A_SPI_LIBRARY/libSPI.a \
+	  lib/WHCS_RF24/WHCS_RF24/libRF24.a \
+	  common/libcommon.a
+CFLAGS=-Wall -Wextra -Werror
 
 # chip and CPU speed information
 MCU=atmega328p
@@ -20,5 +15,18 @@ AVRDUDE_MCU=m328p
 
 # define hardware baud rate
 export USART_BAUD=115200
+
+ifdef MAKE_DOOR
+DEFINES=-DMAKE_DOOR
+endif
+ifdef MAKE_LIGHT
+DEFINES=-DMAKE_LIGHT
+endif
+ifdef MAKE_OUTLET
+DEFINES=-DMAKE_OUTLET
+endif
+ifdef MAKE_TEMP
+DEFINES=-DMAKE_TEMP
+endif
 
 include atmega.mk
--- main.cpp
+++ main.cpp
@@ -13,6 +13,28 @@
 
 #include "pinout.h"
 
+#define BASE_STATION_ID 0xde
+
+#ifdef MAKE_DOOR
+  #pragma message "Making door module"
+  #define CTRL_ID 0
+  #define CTRL_NAME "Door"
+#elif MAKE_LIGHT
+  #pragma message "Making light module"
+  #define CTRL_ID 1
+  #define CTRL_NAME "Light"
+#elif MAKE_TEMP
+  #pragma message "Making temperature module"
+  #define CTRL_ID 2
+  #define CTRL_NAME "Temperature"
+#elif MAKE_OUTLET
+  #pragma message "Making outlet module"
+  #define CTRL_ID 3
+  #define CTRL_NAME "Outlet"
+#else
+  #error "Control module unconfigured!"
+#endif
+
 int uart_putchar(char c, FILE *stream) {
   if (c == '\n')
     uart_putchar('\r', stream);
@@ -27,11 +49,39 @@ int uart_getchar(FILE *stream) {
 }
 
 RF24 rf24(NRF_CE_NUMBER, NRF_CS_NUMBER); // pins on PORTB ONLY
-Radio radio(&rf24, 0x0);
+Radio radio(&rf24, CTRL_ID);
 
 #define MAIN_LOOP_WARNING 300 // 300ms maximum main loop time until warning
 static FILE mystdout;
 static FILE mystdin;
+
+//ISR(INT1_vect)
+//{
+//  radio_pkt pkt;
+//  if(rf24.read(&pkt, MAX_PACKET_SIZE))
+//    if(!radio.queuePacket(&pkt));
+//      //printf("\n");
+//}
+
+// called instead of RESET (vect0) for a bad ISR
+ISR(BADISR_vect)
+{
+  PIN_MODE_OUTPUT(STATUS_LED);
+
+  while(1) {
+    PIN_HIGH(STATUS_LED);
+    _delay_ms(500);
+    PIN_LOW(STATUS_LED);
+    _delay_ms(500);
+  }
+}
+
+// reset the processor
+void reset()
+{
+  // XXX: call reset
+  while(1);
+}
 
 int main()
 {
@@ -46,66 +96,195 @@ int main()
   stdout = &mystdout;
   stdin = &mystdin;
   
-  printf("[W]ireless [H]ome [C]ontrol [S]ystem Control Module\n");
+  printf_P(PSTR("[W]ireless [H]ome [C]ontrol [S]ystem Control Module\n"));
+  printf_P(PSTR("Module name: " CTRL_NAME "\n"));
+  printf_P(PSTR("Module ID: %d\n"), CTRL_ID);
 
   // show that we're powered up
   PIN_MODE_OUTPUT(STATUS_LED);
   PIN_HIGH(STATUS_LED);
 
-  //radio.begin();
+  radio.begin();
+  //radio.enableInterrupt();
+  //rf24.printDetails();
 
   printf_P(PSTR("WHCS main loop starting\n"));
 
   // Enable interrupts before entering main event loop
   sei();
 
+  // keep track of the worst case loop performance
+  Timer tBlink;
   time_t maxLoopTime = 0;
+  Timer loopTimeReset;
+  loopTimeReset.once(5000);
+
+#ifdef MAKE_TEMP
+#define NUM_TEMP_SAMPLES 10
+#define TEMP_UPDATE_INTERVAL 1000
+  int8_t tempAvg = 0;
+  int8_t lastTempAvg = 0;
+  float tempSamples[NUM_TEMP_SAMPLES] = {0};
+  uint8_t tempSamplesHead = 0;
+  uint8_t numSamples = 0;
+  Timer tTemp;
+  tTemp.periodic(TEMP_UPDATE_INTERVAL);
+#endif
 
   // Main event loop
-  Timer evt;
-  evt.periodic(3000);
-
-  radio_pkt pkt;
-
-  PIN_MODE_OUTPUT(DC_RELAY);
-
-  rf24.begin();
-  rf24.openReadingPipe(1, 0xE8E8F0F000LL | 0x00);
-  rf24.startListening();
-  rf24.printDetails();
-
   while(1)
   {
     unsigned long mainStart = millis();
 
-    pkt.size = 1;
-
-    //radio.sendTo(0xe5, &pkt);
-    if(rf24.available())
+    ///////////////////////////////////////
+    // BEGIN MAIN LOOP TIMING
+    ///////////////////////////////////////
+    while(rf24.available())
     {
-      size_t amt = rf24.read(pkt.data, 32);
-
-      printf("Got data %02x amt %u\n", pkt.data[1], amt);
-
-      if(pkt.data[1] == 'O') {
-        printf("ON\n");
-        PIN_HIGH(STATUS_LED);
-        PIN_HIGH(DC_RELAY);
-      }
-      else {
-        printf("OFF\n");
-        PIN_LOW(STATUS_LED);
-        PIN_LOW(DC_RELAY);
-      }
+      //printf("RF AVAIL\n");
+      radio_pkt pkt;
+      if(rf24.read(&pkt, MAX_PACKET_SIZE))
+        if(!radio.queuePacket(&pkt))
+          printf("WARNING: Failed to queue\n");
     }
+
+    while(radio.available())
+    {
+      //printf("RAD AVAIL\n");
+      radio_pkt pkt;
+      radio.recv(&pkt);
+
+      switch(pkt.opcode)
+      {
+        case RADIO_OP_SET_STATUS:
+        {
+          if(pkt.size == 1)
+          {
+            printf(CTRL_NAME ": set status %hu\n", pkt.data[0]);
+
+#ifdef MAKE_DOOR
+            if(pkt.data[0])
+              PIN_HIGH(DC_RELAY);
+            else
+              PIN_LOW(DC_RELAY);
+#elif MAKE_LIGHT || MAKE_OUTLET
+            if(pkt.data[0])
+              PIN_HIGH(AC_RELAY);
+            else
+              PIN_LOW(AC_RELAY);
+#elif MAKE_TEMP
+            printf_P(PSTR("Invalid message for control module type\n"));
+#endif
+          }
+
+          break;
+        }
+        case RADIO_OP_GET_STATUS:
+        {
+#ifdef MAKE_DOOR
+          uint8_t result = PIN_READ(DC_RELAY);
+#elif MAKE_LIGHT || MAKE_OUTLET
+          uint8_t result = PIN_READ(AC_RELAY);
+#elif MAKE_TEMP
+          int8_t result = tempAvg;
+          printf("Temp %d deg F\n", result);
+#endif
+          if(!radio.sendTo(pkt.from, RADIO_OP_SUCCESS_RESULT, (uint8_t*)&result, sizeof(result)))
+          {
+            printf(CTRL_NAME ": get status resp failed\n", pkt.data[0]);
+          }
+
+          break;
+        }
+        case RADIO_OP_PING:
+          if(pkt.from == BASE_STATION_ID)
+          {
+            radio.sendTo(BASE_STATION_ID, RADIO_OP_PONG, NULL, 0);
+          }
+          break;
+        case RADIO_OP_RESTART:
+          if(pkt.from == BASE_STATION_ID)
+          {
+            printf_P(PSTR("Restarting control module...\n"));
+            reset(); // no return
+          }
+          break;
+        default:
+          printf_P(PSTR("Invalid opcode received: %02x\n"), pkt.opcode);
+      }
+
+      // little status blink
+      tBlink.once(100);
+      PIN_LOW(STATUS_LED);
+    }
+
+    if(tBlink.update())
+      PIN_HIGH(STATUS_LED);
+
+#if MAKE_TEMP
+    if(tTemp.update())
+    {
+      // TODO: average temperature
+      int16_t mV = WHCSADC::read(TEMP_SENSOR_NUMBER);
+      float result = (mV*5.0 - 500.0)/10.0;
+
+      tempSamples[tempSamplesHead] = result;
+      numSamples++;
+      numSamples = min(numSamples, NUM_TEMP_SAMPLES);
+
+      float accum = 0;
+
+      for(int i = 0, iter = tempSamplesHead; i < numSamples; i++) {
+        accum += tempSamples[iter];
+        iter--;
+
+        if(iter < 0)
+          iter = NUM_TEMP_SAMPLES-1;
+      }
+      //printf("accum %d, num %d\n", accum, numSamples);
+
+      float avg = (float)accum / numSamples;
+      float avgF = avg*9.0/5 + 32;
+      int8_t outTemp = (int8_t)avgF;
+
+      lastTempAvg = tempAvg;
+      tempAvg = outTemp;
+      printf("Temp %dF, %dC (out %hd)\n", (int)avgF, (int)avg, outTemp);
+      //printf("Listening %d\n", PIN_READ(NRF_CE));
+
+      if(lastTempAvg != tempAvg) // update the base station
+      {
+        // don't care if it gets there
+        radio.sendTo(BASE_STATION_ID, RADIO_OP_NEWDATA, (uint8_t *)&outTemp, sizeof(outTemp));
+      }
+
+      tempSamplesHead = (tempSamplesHead + 1) % NUM_TEMP_SAMPLES;
+    }
+#endif
+
+    ///////////////////////////////////////
+    // END MAIN LOOP TIMING
+    ///////////////////////////////////////
 
     time_t delta = millis() - mainStart;
 
-    if(delta > maxLoopTime) {
-      if(delta > MAIN_LOOP_WARNING)
-        printf("WARNING LIMIT: ");
+    if(loopTimeReset.update())
+      maxLoopTime = 0;
 
-      printf("Main loop max: %lums\n", delta);
+    if(delta > maxLoopTime) {
+      loopTimeReset.once(5000);
+
+      if(delta > MAIN_LOOP_WARNING)
+        printf_P(PSTR("WARNING LIMIT REACHED: "));
+
+      printf_P(PSTR("Main loop max: %lums"), delta);
+      //rf24.printDetails();
+      //rf24.startListening();
+
+      if(maxLoopTime > 0)
+        printf_P(PSTR(" (+%lums)\n"), delta-maxLoopTime);
+      else
+        printf_P(PSTR("\n"));
 
       maxLoopTime = delta;
     }
